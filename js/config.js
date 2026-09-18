7fea11c6d9e6d19e85837a7a3f0f52c6954ff8da
AgustinRoss39 <104650789+AgustinRoss39@users.noreply.github.com>
2026-09-18 17:56:01 -0300

    Centraliza configuración de invitación Osito Aviador

---
+++ js/config.js
@@ -0,0 +1,48 @@
+window.INVITATION_CONFIG = {
+  invitation: {
+    name: "Leandro",
+    ageLabel: "CUMPLE 1 AÑO",
+    message: "¡Es mi primer cumpleaños! Vamos a divertirnos celebrando juntos.",
+    eventDateTime: "2026-11-21T18:00:00-03:00",
+    eventEndDateTime: "2026-11-21T21:00:00-03:00",
+    dateLabel: "Sábado 21 de Noviembre",
+    timeLabel: "18:00 a 21:00 hs.",
+    venue: "Celebra Eventos",
+    address: "Av. Manuel Belgrano 1340, Crucecita, Avellaneda",
+    mapUrl: "https://www.google.com/maps/search/?api=1&query=Celebra+Eventos+Av.+Manuel+Belgrano+1340+Crucecita+Avellaneda",
+    confirmationNote: "Necesitamos tu confirmación antes del 14 de noviembre.",
+    whatsappNumber: "5491156223007",
+    whatsappMessage: "¡Hola! Confirmo mi asistencia al cumpleaños de Leandro. 🧸✈️",
+    instagramUrl: "https://www.instagram.com/rossdigitalstudio/",
+    instagramHandle: "@rossdigitalstudio",
+    audioUrl: "https://raw.githubusercontent.com/DigitalRoss/invitacionOsito/main/www.tarjetas.cool/modelos/cumple/osito_aviador/sounds/audio.mp3",
+    portraitUrl: "https://raw.githubusercontent.com/DigitalRoss/invitacionOsito/main/www.tarjetas.cool/modelos/cumple/osito_aviador/img/foto1.jpg",
+    ribbonUrl: "https://raw.githubusercontent.com/DigitalRoss/invitacionOsito/main/www.tarjetas.cool/modelos/cumple/osito_aviador/img/cinta.png"
+  },
+  characters: [
+    {
+      src: "https://raw.githubusercontent.com/DigitalRoss/invitacionOsito/main/www.tarjetas.cool/modelos/cumple/osito_aviador/img/2.png",
+      width: "min(98vw, 445px)",
+      top: "24px",
+      translateX: "-50%"
+    },
+    {
+      src: "https://raw.githubusercontent.com/DigitalRoss/invitacionOsito/main/www.tarjetas.cool/modelos/cumple/osito_aviador/img/1.png",
+      width: "min(104vw, 470px)",
+      top: "30px",
+      translateX: "-50%"
+    },
+    {
+      src: "https://raw.githubusercontent.com/DigitalRoss/invitacionOsito/main/www.tarjetas.cool/modelos/cumple/osito_aviador/img/3.png",
+      width: "min(101vw, 455px)",
+      top: "34px",
+      translateX: "-50%"
+    },
+    {
+      src: "https://raw.githubusercontent.com/DigitalRoss/invitacionOsito/main/www.tarjetas.cool/modelos/cumple/osito_aviador/img/4.png",
+      width: "min(88vw, 400px)",
+      top: "10px",
+      translateX: "-50%"
+    }
+  ]
+};
